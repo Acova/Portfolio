8d83a13db11b7e345a1be29403b0860e94c48ba7
Héctor <hector.acosta.valido@outlook.es>
2021-02-26 12:21:44 +0000

    Still having probles with animations, will try another solution

--- js/carousel.js
+++ js/carousel.js
@@ -7,13 +7,16 @@ const findByClassName = (className) => {
 }
 
 const configButtons = (titles) => {
+    console.log(titles);
     configNextButton(titles);
     configPreviousButton(titles);
 }
 
 const configNextButton = (titles) => {
+    console.log(titles);
     let previousIndex = titles.findIndex(elem => elem.classList.contains('previous-title'));
     let prev_title_btn = document.getElementsByClassName('carousel-btn-prev')[0];
+    console.log(previousIndex);
     if(previousIndex === -1) {
         prev_title_btn.disabled = true;
     } else {
@@ -24,11 +27,20 @@ const configNextButton = (titles) => {
 const configPreviousButton = (titles) => {
     let nextIndex =  titles.findIndex(elem => elem.classList.contains('next-title'));
     let next_title_btn = document.getElementsByClassName('carousel-btn-next')[0];
+    console.log(nextIndex);
     if(nextIndex === -1) {
         next_title_btn.disabled = true;
     } else {
         next_title_btn.disabled = false;
     }
+}
+
+const disableButtons = () => {
+    let btn_prev = findByClassName('carousel-btn-prev')[0];
+    let btn_next = findByClassName('carousel-btn-next')[0];
+    
+    btn_prev.disabled = true;
+    btn_next.disabled = true;
 }
 
 const init = () => {
@@ -90,14 +102,24 @@ const deleteFodder = () => {
     }
 }
 
+const endAnimation = (e) => {
+    let titles = findByClassName('carousel-title');
+    configButtons(titles);
+    e.target.removeEventListener('animated', endAnimation, false);
+}
+
 const nextSlider = () => {
     let titles = findByClassName('carousel-title');
     let elems = findByClassName('carousel-elem');
+    let carousel = findByClassName('carousel')[0];
 
+    
+    carousel.addEventListener('animated', endAnimation, false);
+    
+    disableButtons();
     deleteFodder();
     nextTitle(titles);
-    nextElem(elems);
-    configButtons(titles);
+    nextElem(elems);    
 }
 
 const nextTitle = (titles) => {
@@ -109,7 +131,6 @@ const nextTitle = (titles) => {
         previous.classList.add('sideToNone');
         previous.addEventListener('animationend', e => {
             previous.classList.remove('previous-title');
-            configButtons(titles);
         })
     }
 
@@ -121,7 +142,6 @@ const nextTitle = (titles) => {
     current.addEventListener('animationend', e => {
         current.classList.remove('current-title');
         current.classList.add('previous-title');
-        configButtons(titles);
     });
 
     // We set the next title to the current title
@@ -132,12 +152,15 @@ const nextTitle = (titles) => {
     next.addEventListener('animationend', e => {
         next.classList.remove('next-title');
         next.classList.add('current-title');
-        configButtons(titles);
+        let event = new CustomEvent('animated');
+        let carousel = findByClassName('carousel')[0]; 
+        carousel.dispatchEvent(event);
     });
 
     // We must make sure there are more elements
     if((titles.length - 1) > nextIndex){
         let newNext = titles[nextIndex + 1];
+        newNext.classList.remove('sideToNone');
         newNext.classList.add('noneToSide', 'next-title');
     } else {
         addTitleFodderAfter();
@@ -155,11 +178,14 @@ const nextElem = (elems) => {
 const previousSlider = () => {
     let titles = findByClassName('carousel-title');
     let elems = findByClassName('carousel-elem');
+    let carousel = findByClassName('carousel')[0];
 
+    carousel.addEventListener('animated', endAnimation, false);
+
+    disableButtons();
     deleteFodder();
     previousTitle(titles);
     previousElem(elems);
-    configButtons(titles);
 }
 
 const previousTitle = (titles) => {
@@ -167,27 +193,43 @@ const previousTitle = (titles) => {
     let nextIndex = titles.findIndex(elem => elem.classList.contains('next-title'));
     if(nextIndex !== -1) {
         let next = titles[nextIndex];
-        next.classList.remove('next-title');
+        next.classList.remove('currentToSide', 'noneToSide');
+        next.classList.add('sideToNone');
+        next.addEventListener('animationend', e => {
+            next.classList.remove('next-title');
+        });
     }
 
     // We set the current title to the previous title
     let currentIndex = titles.findIndex(elem => elem.classList.contains('current-title'));
     let current = titles[currentIndex];
-    current.classList.remove('current-title');
-    current.classList.add('next-title');
+    current.classList.remove('sideToCurrent');
+    current.classList.add('currentToSide');
+    current.addEventListener('animationend', e => {
+        current.classList.remove('current-title');
+        current.classList.add('next-title');
+    });
 
     // We modify the "previous" title
     let previousIndex = titles.findIndex(elem => elem.classList.contains('previous-title'));
     let previous = titles[previousIndex];
-    previous.classList.remove('previous-title');
-    previous.classList.add('current-title');
+    previous.classList.remove('noneToSide', 'currentToSide')
+    previous.classList.add('sideToCurrent');
+    previous.addEventListener('animationend', e => {
+        previous.classList.remove('previous-title');
+        previous.classList.add('current-title');
+        let event = new CustomEvent('animated');
+        let carousel = findByClassName('carousel')[0]; 
+        carousel.dispatchEvent(event);
+    });
 
     // We must make sure there are more elements
     if(previousIndex === 0) {
-        addTitlesFodder(titles);
+        addTitleFodderBefore();
     } else {
         let newPrev = titles [previousIndex - 1];
-        newPrev.classList.add('previous-title');
+        newPrev.classList.remove('sideToNone');
+        newPrev.classList.add('noneToSide', 'previous-title');
     }
 
 }
